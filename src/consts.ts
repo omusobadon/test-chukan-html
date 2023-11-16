42d59c3cc384ee6583f049378415606b9e0e7a12
yaffu-kitune <22cc0203@jec.ac.jp>
2023-11-17 04:21:48 +0900

    Upload File

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'オムそば丼オフィシャルサイト';
+export const SITE_DESCRIPTION = 'オムそば丼のオフィシャルサイトです。';
---
+++ src/content/config.ts
@@ -0,0 +1,43 @@
+import { defineCollection, z } from "astro:content";
+
+const blog = defineCollection({
+  type: "content",
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    // Transform string to Date object
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+    heroImage: z.string().optional(),
+  }),
+});
+// docs folder is the root of the collection
+// 1. ユーティリティを`astro:content`からインポート
+
+// 2. 各コレクションに`type`と`schema`を定義
+const Guide = defineCollection({
+  type: "content", // v2.5.0以降
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    // Transform string to Date object
+  }),
+});
+
+const Reference = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    // Transform string to Date object
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+  }),
+});
+
+export const collections = {
+  blog: blog,
+  docs: Guide,
+  reference: Reference,
+};
