46080400afbce3656e87b9a7d1e92ffed033709d
yaffu-kitune <22cc0203@jec.ac.jp>
2023-11-21 00:16:50 +0900

    edit docs

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,28 +1,14 @@
-import { defineCollection, z } from "astro:content";
-
-// 2. 各コレクションに`type`と`schema`を定義
-const Guide = defineCollection({
-  type: "content", // v2.5.0以降
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    tags: z.string()
-    // Transform string to Date object
-  }),
+// Import utilities from `astro:content`
+import { z, defineCollection } from "astro:content";
+// Define a `type` and `schema` for each collection
+const postsCollection = defineCollection({
+    type: 'content',
+    schema: z.object({
+      title: z.string(),
+      description: z.string(),
+    })
 });
-
-
-const Reference = defineCollection({
-  type: "content",
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    tags: z.string(),    
-  }),
-});
-
+// Export a single `collections` object to register your collection(s)
 export const collections = {
-  guides: Guide,
-  references:Reference
-  
+  guides: postsCollection,
 };
