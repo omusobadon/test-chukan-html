dce5cf8e41ebc3eb45a9859f80a380fb46419feb
yaffu-kitune <22cc0203@jec.ac.jp>
2023-11-20 20:15:14 +0900

    edit docs

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,28 +1,15 @@
 import { defineCollection, z } from "astro:content";
 
-const blog = defineCollection({
-  type: "content",
-  // Type-check frontmatter using a schema
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    // Transform string to Date object
-    pubDate: z.coerce.date(),
-    updatedDate: z.coerce.date().optional(),
-    heroImage: z.string().optional(),
-  }),
-});
-
 // 2. 各コレクションに`type`と`schema`を定義
 const Guide = defineCollection({
   type: "content", // v2.5.0以降
   schema: z.object({
     title: z.string(),
     description: z.string(),
+    tags: z.string()
     // Transform string to Date object
   }),
 });
-
 
 
 const Reference = defineCollection({
@@ -30,14 +17,12 @@ const Reference = defineCollection({
   schema: z.object({
     title: z.string(),
     description: z.string(),
-    // Transform string to Date object
-    pubDate: z.coerce.date(),
-    updatedDate: z.coerce.date().optional(),
+    tags: z.string(),    
   }),
 });
 
 export const collections = {
-  blog: blog,
-  docs: Guide,
+  guides: Guide,
+  references:Reference
   
 };
